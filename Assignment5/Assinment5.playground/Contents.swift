2381fbf9a989de3d055582a14a2d2895415c4a95
Keshav Aggarwal <keshavaggarwaldps@yahoo.com>
2016-07-21 19:08:57 -0400

    Assignment 5: Final 1

--- Assignment5/Assinment5.playground/Contents.swift
+++ Assignment5/Assinment5.playground/Contents.swift
@@ -1,3 +1,8 @@
+/*
+ Made By: Keshav Aggarwal
+ Date   : 07/19/2016
+*/
+
 import UIKit
 
 let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -8,16 +13,16 @@ func isLeap(year: Int) ->  Bool {
 
 func julianDate(year: Int, month: Int, day: Int) -> Int {
     let yearDays = (1900..<year).reduce(0) { isLeap($1) ? $0 + 366 : $0 + 365 }
-    let monthDays = (1..<month).reduce(0) { $0 + months[$1] }
+    let monthDays = (1..<month).reduce(0) { $0 + months[$1 - 1] }
     return yearDays + monthDays + day + (month > 2 ? isLeap(year) ? 1 : 0 : 0)
 }
 
 julianDate(1960, month:  9, day: 28)
 julianDate(1900, month:  1, day: 1)
 julianDate(1900, month: 12, day: 31)
-julianDate(1901, month: 1, day: 1)
-julianDate(1901, month: 1, day: 1) - julianDate(1900, month: 1, day: 1)
-julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
+julianDate(1901, month:  1, day: 1)
+julianDate(1901, month:  1, day: 1) - julianDate(1900, month: 1, day: 1)
+julianDate(2001, month:  1, day: 1) - julianDate(2000, month: 1, day: 1)
 
 isLeap(1960)
 isLeap(1900)
